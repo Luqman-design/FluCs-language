e8ca8bff4fc68ea43f0e46b8e60b8b311e2867a4
Philippe Bourrachot <rd57yh@student.aau.dk>
2026-02-27 12:13:37 +0100

    More progress on lexer

--- main.c
+++ main.c
@@ -2,100 +2,129 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-typedef enum {
-    INT, IDENTIFIER, EQUAL, SEMICOLON, INT_VALUE
+typedef enum
+{
+	INT,
+	IDENTIFIER,
+	EQUAL,
+	SEMICOLON,
+	INT_VALUE
 } TokenType;
 
-typedef enum {
-    START, IN_INTEGER, IN_IDENTIFIER, ACCEPT
+typedef enum
+{
+	START,
+	IN_INTEGER,
+	IN_IDENTIFIER,
+	ACCEPT
 } State;
 
-typedef struct {
-    TokenType type;
-    union {
-        char *string_value;
-        int int_value;
-    } value;
+typedef struct
+{
+	TokenType type;
+	union
+	{
+		char *string_value;
+		int int_value;
+	} value;
 } Token;
 
-typedef struct {
-    char *input;
-    int position;
+typedef struct
+{
+	char *input;
+	int position;
 } Lexer;
 
-Lexer new_lexer(char* input) {
-    Lexer lexer;
-    lexer.input = input;
-    lexer.position = 0;
-    return lexer;
+Lexer new_lexer(char *input)
+{
+	Lexer lexer;
+	lexer.input = input;
+	lexer.position = 0;
+	return lexer;
 }
 
-char peek(Lexer *lexer) {
-	return lexer -> input[lexer -> position];
+char peek(Lexer *lexer)
+{
+	return lexer->input[lexer->position];
 }
 
-Token next_token(Lexer lexer) {
+Token next_token(Lexer lexer)
+{
 	State state = START;
-	char current_char = peek(&lexer);
 	char temp_token[64];
 	int temp_token_tracker = 0;
 
-	while(state != ACCEPT) {
-		if (state == START && current_char == '=') {
+	while (state != ACCEPT)
+	{
+		char current_char = peek(&lexer);
+		if (state == START && current_char == '=')
+		{
 			Token token;
 			token.type = EQUAL;
 			lexer.position++;
 			return token;
-		} else if (state == START && current_char == ';') {
+		}
+		else if (state == START && current_char == ';')
+		{
 			Token token;
 			token.type = SEMICOLON;
 			lexer.position++;
 			return token;
-		} else if (state == START && (current_char == '_' || isalpha(current_char))) {
+		}
+		else if (state == START && (current_char == '_' || isalpha(current_char)))
+		{
 			temp_token[temp_token_tracker] = current_char;
 			temp_token_tracker++;
 			lexer.position++;
 			state = IN_IDENTIFIER;
-		} else if (state == IN_IDENTIFIER && (current_char == '_' || isalpha(current_char))) {
+		}
+		else if (state == IN_IDENTIFIER && (current_char == '_' || isalpha(current_char)))
+		{
 			temp_token[temp_token_tracker] = current_char;
 			temp_token_tracker++;
 			lexer.position++;
-		} else if (state == IN_IDENTIFIER) {
+		}
+		else if (state == IN_IDENTIFIER)
+		{
 			temp_token[temp_token_tracker] = current_char;
 			temp_token_tracker++;
+			temp_token[temp_token_tracker] = '\0';
 			lexer.position++;
-			
+
 			Token token;
 			token.type = IDENTIFIER;
 			token.value.string_value = temp_token;
-			printf("%s", temp_token);
+			printf("%s\n", temp_token);
 			return token;
-		} else if (state == START && isdigit(current_char)) { 
+		}
+		else if (state == START && isdigit(current_char))
+		{
 			temp_token[temp_token_tracker] = current_char;
 			temp_token_tracker++;
 			lexer.position++;
 			state = IN_INTEGER;
+		}
+		else if (state == IN_INTEGER && isdigit(current_char))
+		{
+			temp_token[temp_token_tracker] = current_char;
+			temp_token_tracker++;
+			lexer.position++;
+		}
+		else if (state == IN_INTEGER)
+		{
+			temp_token[temp_token_tracker] = current_char;
+			temp_token_tracker++;
+			temp_token[temp_token_tracker] = '\0';
+			lexer.position++;
 
-		} else if (state == IN_INTEGER && isdigit(current_char)) {
-			temp_token[temp_token_tracker] = current_char;
-			temp_token_tracker++;
-			lexer.position++;
-		} else if (state == IN_INTEGER) {
-			temp_token[temp_token_tracker] = current_char;
-			temp_token_tracker++;
-			lexer.position++;
-			
 			Token token;
 			token.type = INT_VALUE;
 			token.value.int_value = atoi(temp_token);
 			return token;
 		}
-
-		
 	}
-		Token token;
-		return token;
-
+	Token token;
+	return token;
 }
 
 /*
@@ -106,7 +135,7 @@ Token next_token(Lexer lexer) {
  * struct:
  *    input (vores kode string),
  *    position (hvilket char er vi ved)
- * 
+ *
  * Lexer new_lexer(char* input)
  *    Lav ny lexer of retuner den
  *
@@ -143,12 +172,13 @@ Token next_token(Lexer lexer) {
  *              så lav den til et keyword i stedet.
  */
 
-int main() {
-    printf("Hello, World!\n");
-    char *str = "int x = 10;";
-    printf("%s", str);
+int main()
+{
+	printf("Hello, World!\n");
+	char *str = "int x = 10;\n";
+	printf("%s", str);
 	Lexer lexer = new_lexer(str);
 	Token token = next_token(lexer);
 
-    return 0;
+	return 0;
 }
