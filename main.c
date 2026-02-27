1bad6648e520933840a7c61eaa475b5d095dffcf
username <villi05.v.j@gmail.com>
2026-02-27 09:31:49 +0100

    setup

--- main.c
+++ main.c
@@ -1,17 +1,61 @@
 #include <stdio.h>
 
-enum Token{
+enum TokenType {
     INT, IDENTIFIER, EQUAL, SEMICOLON, INT_VALUE
 };
 
-enum State{
-    START, ININTEGER, ACCEPT
+enum State {
+    START, IN_INTEGER, IN_IDENTIFIER, ACCEPT
 };
 
 typedef enum Token Token;
 
 typedef enum State State;
 
+/*
+ * struct:
+ *    type,
+ *    værdi (enten string eller int) (hint: brug union)
+ *
+ * struct:
+ *    input (vores kode string),
+ *    position (hvilket char er vi ved)
+ * 
+ * Lexer new_lexer(char* input)
+ *    Lav ny lexer of retuner den
+ *
+ * Token next_token(Lexer lexer)
+ *    loop (stop loopet ved ACCEPT state)
+ *
+ *        if state is START and char == '='
+ *            return Token(TokenType.EQUAL)
+ *        if state is START and char == ';'
+ *            return ...
+ *
+ *
+ *        if state is START and if a-z | A-Z | _
+ *            set state IN_IDENTIFIER
+ *
+ *        if state is IN_IDENTIFIER and if a-z | A-Z | _
+ *            set state IN_IDENTIFIER
+ *
+ *        if state is IN_IDENTIFIER
+ *            set state ACCEPT
+ *
+ *
+ *        if state is START and if 0-9
+ *            set state IN_INTEGER
+ *
+ *        if state is IN_INTEGER and if 0-9
+ *            set state IN_INTEGER
+ *
+ *        if state is IN_INTEGER
+ *            set state ACCEPT
+ *
+ *    switch på Identifier'en
+ *        hvis den matcher et keyword (som 'int'),
+ *              så lav den til et keyword i stedet.
+ */
 
 int main() {
     printf("Hello, World!\n");
