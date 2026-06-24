24b5c73525d9988d1c93d095b4911326566e329b
philippeysl20 <philipbourrachot@gmail.com>
2026-06-24 12:18:40 +0200

    forcing a bug in case3 for testing

--- temp.c
+++ temp.c
@@ -5,13 +5,10 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/mman.h>
-void process_call_1(int*, intptr_t*);
-void process_call_2(int*, intptr_t*);
-int add(int a, int b) {return (a+b);}
-
-void process_call_1(int* result, intptr_t* args) {*result=add((int)args[0], (int)args[1]);exit(0);}
-void process_call_2(int* result, intptr_t* args) {*result=add((int)args[0], (int)args[1]);exit(0);}
-int main() {
-int total = 0;if (1){intptr_t _args_p1[2]={10, 5};int r1 = 0; int* r1_ptr = mmap(NULL, sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0); *r1_ptr = 0; pid_t _process_r1 = fork(); if (_process_r1 == 0) { process_call_1(r1_ptr, _args_p1); }intptr_t _args_p2[2]={20, 3};int r2 = 0; int* r2_ptr = mmap(NULL, sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0); *r2_ptr = 0; pid_t _process_r2 = fork(); if (_process_r2 == 0) { process_call_2(r2_ptr, _args_p2); }waitpid(_process_r1, NULL, 0); r1 = *r1_ptr;waitpid(_process_r2, NULL, 0); r2 = *r2_ptr;total=(r1+r2);}printf("%d",total);
+int counter = 0;
+pthread_mutex_t lock_counter;
+void *for_loop_worker_1(void *arg) { intptr_t* _args = (intptr_t*)arg; int start_index = (int)_args[0] + 0; int* _counter = (int*)_args[1]; pthread_mutex_t* _lock_counter = (pthread_mutex_t*)_args[2]; for (int i = start_index; i < 100; i = i + 3) { pthread_mutex_lock(_lock_counter);printf("%d",i);pthread_mutex_lock(_lock_counter);(*_counter)=((*_counter)+i);pthread_mutex_unlock(_lock_counter);printf("%s","\n");pthread_mutex_unlock(_lock_counter);} return NULL; }int main() {
+pthread_mutex_init(&lock_counter, NULL);
+pthread_mutex_t _lock_counter;pthread_mutex_init(&_lock_counter, NULL);intptr_t _fargs[3][3]; for (int _fi = 0; _fi < 3; _fi++) { _fargs[_fi][0] = _fi; _fargs[_fi][1] = (intptr_t)&counter; _fargs[_fi][2] = (intptr_t)&_lock_counter; } pthread_t threads[3]; for (int i = 0; i < 3; i++) { pthread_create(&threads[i], NULL, for_loop_worker_1, _fargs[i]); } for (int i = 0; i < 3; i++) { pthread_join(threads[i], NULL); }pthread_mutex_destroy(&_lock_counter);
   return 0;
 }
